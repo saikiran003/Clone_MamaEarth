d46c330e58057c74f07501dd1666751ffcbb4492
Bharani619 <bharanish619@gmail.com>
2022-05-03 12:05:03 +0530

    sample data added

---
+++ Datas/HomePage.js
@@ -0,0 +1,426 @@
+const BestSeller = [ 
+       {
+          "id":1,
+          "name":"",
+          "pImg":"",
+          "pShortDec":"",
+          "rating":"",
+          "reviews":"",
+          "sellerTag":"",
+          "category":""
+
+       },
+
+       {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+
+]
+
+const OnionRange = [
+
+    {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+]
+
+const VitaminRange = [
+    {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+
+]
+
+const UbtanRange = [
+
+
+    {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+]
+
+const Newlaunches = [
+
+    {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+]
+
+const summerFav = [
+
+    {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+     {
+        "id":1,
+        "name":"",
+        "pImg":"",
+        "pShortDec":"",
+        "rating":"",
+        "reviews":"",
+        "sellerTag":"",
+        "category":""
+
+     },
+
+]
