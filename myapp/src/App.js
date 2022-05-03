6e12fe72f7c507a3f2a03809856ff35ac4673f0a
sachiningale1998 <99962904+sachiningale1998@users.noreply.github.com>
2022-05-04 02:31:54 +0530

    Navbar

---
+++ myapp/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainRoutes from './Mainroutes/MainRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <MainRoutes/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Mainroutes/MainRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../Header/Header'
+
+const MainRoutes = () => {
+  return (
+    <div>
+        <Header/>
+    </div>
+  )
+}
+
+export default MainRoutes
